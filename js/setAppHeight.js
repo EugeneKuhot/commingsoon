940caee3206f914c26e6433ec53aa458218d6e4a
Eugene Kuhot <42823792+EugeneKuhot@users.noreply.github.com>
2025-07-14 12:12:13 +0200

    fix email

--- js/setAppHeight.js
+++
@@ -1,6 +0,0 @@
-function setInitialAppHeight() {
-    const appHeight = window.innerHeight;
-    document.documentElement.style.setProperty('--app-height', `${appHeight}px`);
-}
-
-window.addEventListener('DOMContentLoaded', setInitialAppHeight);
